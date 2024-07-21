144d7eafdf8f1efa9b3c8bbefd9ff63983c56e3a
idle <meta456trader@gmail.com>
2024-07-21 09:55:27 -0700

    install plugins

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,14 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `idle-blog`,
+    title: `meta456trader's portfolio`,
     siteUrl: `https://www.yourdomain.tld`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: ["gatsby-plugin-postcss","gatsby-plugin-image","gatsby-image-sharp"]
 };
 
 export default config;
